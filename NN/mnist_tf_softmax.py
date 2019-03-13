f4a28889a692a413b237b99bc529b27d7fe6ba43
huhb(胡辉滨) <huhb@500wan.com>
2019-03-13 14:37:12 +0800

    tensorflow 多元函数交叉熵实现mnist

---
+++ NN/mnist_tf_softmax.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+mnist_data = input_data.read_data_sets("./MNIST_DATA/", one_hot=True)
+#dir(mnist_data.train)
+
+mnist_data.train.images
+mnist_data.train.labels
+
+images_0 = mnist_data.train.images[0]
+labels_0 = mnist_data.train.labels[0]
+
+
+def show_img(image, label):
+    image_reshape = image.reshape(28,28)
+    img_0 = Image.fromarray(image_reshape*255)
+    print('label:%s' % np.argmax(label))
+    plt.imshow(img_0)
+    
+show_img(mnist_data.train.images[7], mnist_data.train.labels[7])
+
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+x = tf.placeholder(tf.float32, [None, 784])
+
+y = tf.nn.softmax(tf.matmul(x,W)+b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = -tf.reduce_mean(y_*tf.log(y))
+optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.arg_max(y,1), tf.arg_max(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+init = tf.global_variables_initializer()
+
+saver = tf.train.Saver(max_to_keep=1)
+
+epoches = 10000
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(epoches):
+        batch_xs, batch_ys = mnist_data.train.next_batch(100)
+        sess.run(optimizer, feed_dict={x: batch_xs, y_: batch_ys})
+        if epoch % 100 == 0:
+            print('epoch:%s,acc:%s' % (epoch, sess.run(accuracy, 
+                feed_dict={x: mnist_data.test.images, y_: mnist_data.test.labels})))
+    saver.save(sess, './ckpt/mnist.ckpt')
+            
+
+isess = tf.InteractiveSession()
+
+model = tf.train.latest_checkpoint('./ckpt')
+saver.restore(isess, model)
+
+def prediction(n=0):
+    test_img = mnist_data.test.images[n]
+    test_label = mnist_data.test.labels[n]
+    show_img(test_img, test_label)
+    y_r = isess.run(y, feed_dict={x: [test_img]})
+    print('prediction:%s' % (np.argmax(y_r,1)[0]))
+
+prediction(133)
+
+
+
+
+#print(isess.run(tf.arg_max([[1,2,3]],0)))
+#print(isess.run(tf.arg_max([[1,2,3],[2,2,4]],0)))
+#print(isess.run(tf.arg_max([[1,2,3],[2,2,4]],1)))
