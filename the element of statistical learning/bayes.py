7cd3eb96b067cf20313d4bfd0e895b41139b618b
huhb(胡辉滨) <huhb@500wan.com>
2019-03-14 15:06:40 +0800

    sklearn高斯贝叶斯

---
+++ the element of statistical learning/bayes.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+from collections import Counter
+import math
+
+def create_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['label'] = iris.target
+    df.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'label']
+    data = np.array(df.iloc[:100, :])
+    # print(data)
+    return data[:,:-1], data[:,-1]
+
+X, y = create_data()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+
+from sklearn.naive_bayes import GaussianNB
+
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+
+clf.score(X_test, y_test)
+clf.predict([[4.4,  3.2,  1.3,  0.2]])
+
